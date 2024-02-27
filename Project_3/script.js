c31c1b07164c2d17332f210a8eefbd64c19e3683
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-02-27 23:25:32 +0530

    Updated Script

--- Project_3/script.js
+++ Project_3/script.js
@@ -1,6 +1,6 @@
 const clock = document.getElementById('clock');
 
 setInterval(function () {
-    let date = new Date();
+    let date = new Date();           
     clock.innerHTML = date.toLocaleTimeString();
 }, 1000);
